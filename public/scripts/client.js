b8deadda657b69bb7aedf8d7da1c7814d708b9eb
caro-turnbull <caro.turnbull@gmail.com>
2023-06-22 14:25:00 -0400

    hover over icons and over tweet articles, problem with looping posts

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-//const { response } = require("express");
-
-
-// const tweetData = 
-//   [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png",
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1687196906305
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@rd"
-//       },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1687283306305
-//     }
-//   ]
-
 
 $(document).ready(function(){
   
@@ -96,19 +67,16 @@ $(document).ready(function(){
     }
     })
    }
-  loadTweets()
+  //loadTweets()
   
 
   //listener for button
   $('#tweet-form').on("submit", function (event) {
     event.preventDefault();
-    console.log("button pushed!")
     
     const tweetContent = document.getElementById("tweet-text")
     const tweetText = tweetContent.value;  
-    console.log("text=", tweetText)
     
-  
     if (!tweetText){        
       alert("Your tweet is empty.")
       return
@@ -127,7 +95,6 @@ $(document).ready(function(){
         success: function() {
           console.log("Data submitted successfully");
           loadTweets();
-          $("form#tweet-form").val();
         },
         error: function() {
           console.error("Error submitting data");
